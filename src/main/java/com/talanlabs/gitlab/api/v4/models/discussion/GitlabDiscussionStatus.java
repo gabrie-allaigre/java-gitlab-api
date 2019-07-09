e1a5f51bd3f11f1dca37952925e8fa661dfea2f8
sunyanling <sunyanling@zhihu.com>
2019-07-09 17:36:40 +0800

    append get method in GitlabDiscussionStatus

--- src/main/java/com/talanlabs/gitlab/api/v4/models/discussion/GitlabDiscussionStatus.java
+++ src/main/java/com/talanlabs/gitlab/api/v4/models/discussion/GitlabDiscussionStatus.java
@@ -7,10 +7,22 @@ import java.util.List;
 
 public class GitlabDiscussionStatus {
 
-    private Integer id;
+    private String id;
 
     @JsonProperty("individual_note")
     private boolean individualNote;
 
     private List<GitlabNote> notes;
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean getIndividualNote() {
+        return individualNote;
+    }
+
+    public List<GitlabNote> getNotes() {
+        return notes;
+    }
 }
